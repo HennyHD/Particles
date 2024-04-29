41b7ab859cf2c2467c7e81a51a63baa384c133ff
HennyHD <HHuyD1@gmail.com>
2024-04-28 23:08:25 -0700

    update

--- codes/Engine.cpp
+++ codes/Engine.cpp
@@ -13,12 +13,5 @@ Engine::Engine()
 
 	Particle particle();
 	
-	//tester
-	cout << "Starting Particle unit tests..." << endl;
-	Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
-	p.unitTests();
-	cout << "Unit tests complete.  Starting engine..." << endl;
-
-
 		
 }
